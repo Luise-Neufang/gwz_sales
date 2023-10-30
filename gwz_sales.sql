8d7312e3d2ab71b80db7455ec3d7343dbdf3de74
Luise-Neufang <luise.neufang@gmail.com>
2023-10-30 15:54:07 +0100

    round_order

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT
 date_date,
-SUM(turnover) AS total_turnover,
-SUM (purchase_cost) AS purchase_cost
+ROUND(SUM(turnover),2) AS total_turnover,
+ROUND(SUM(purchase_cost),2) AS purchase_cost
 FROM `luise-neufang.github1.gwz_sales`
 GROUP BY date_date
-
+ORDER BY date_date ASC
